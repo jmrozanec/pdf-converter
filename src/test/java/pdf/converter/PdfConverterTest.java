04d30ad3937b7f1a69df4898e957eae4eb9b66e0
Jose Martin Rozanec <jmrozanec@gmail.com>
2018-10-14 14:18:53 +0200

    Add test

---
+++ src/test/java/pdf/converter/PdfConverterTest.java
@@ -0,0 +1,34 @@
+package pdf.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+public class PdfConverterTest {
+
+    @Test
+    public void convert() throws Exception {
+        Path path = copyToTmp();
+        File dest = new File(path.toFile(), "mobydick.epub");
+        PdfConverter
+                .convert(new File(path.toFile(), "mobydick.pdf"))
+                .intoEpub("Moby Dick", dest);
+        Assert.assertTrue(dest.exists());
+    }
+
+    private Path copyToTmp() throws IOException {
+        Path tmpdir = new File(String.format("/tmp/%s", UUID.randomUUID().toString())).toPath();
+        Files.createDirectories(tmpdir);
+        Path dest = new File(tmpdir.toFile(), "mobydick.pdf").toPath();
+        ClassLoader classLoader = getClass().getClassLoader();
+        Path file = new File(classLoader.getResource("mobydick.pdf").getFile()).toPath();
+        Files.copy(file, dest, StandardCopyOption.REPLACE_EXISTING);
+        return tmpdir;
+    }
+}
